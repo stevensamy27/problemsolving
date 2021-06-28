9261ea260e9650c1c7fd786638f0efd5239349c6
Steven Samy <stevensivo27@gmail.com>
2021-06-28 18:36:06 +0200

    Adding solution for 71A Problem on Codeforces

--- codeforces/71A.py
+++ codeforces/71A.py
@@ -1,1 +1,12 @@
-print("Hello world")
+n = int(input())
+
+
+def solve(s):
+    if len(s) <= 10:
+        return s
+
+    return s[0]+str(len(s)-2)+s[len(s)-1]
+
+for i in range(n):
+    s = input()
+    print(solve(s))
