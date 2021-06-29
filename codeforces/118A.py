04e5d36fcc9dde41f344408313a071c86f1407e2
Steven Samy <stevensivo27@gmail.com>
2021-06-29 12:51:22 +0200

    Solving 118A Problem from Codeforces

--- codeforces/118A.py
+++ codeforces/118A.py
@@ -1,1 +1,15 @@
-print("Hello world")
+n = input()
+vowels = ["a", "o", "y", "e", "u", "i"] 
+
+val = ""
+for i in n:
+    i = i.lower()
+    partial = ""
+    if i not in vowels:
+        partial = "."+i
+    else:
+        partial = ""
+    
+    if partial:
+        val += partial
+print(val)
