3347e0b4b50a93c0bb9534e0682b15e2bf5e0cba
Steven Samy <stevensivo27@gmail.com>
2021-06-29 14:05:00 +0200

    solving 118A problem with best solution

--- codeforces/118A.py
+++ codeforces/118A.py
@@ -2,14 +2,9 @@ n = input()
 vowels = ["a", "o", "y", "e", "u", "i"] 
 
 val = ""
-for i in n:
+for  i in n:
     i = i.lower()
-    partial = ""
+
     if i not in vowels:
-        partial = "."+i
-    else:
-        partial = ""
-    
-    if partial:
-        val += partial
-print(val)
+        print("."+ i , end='')
+
