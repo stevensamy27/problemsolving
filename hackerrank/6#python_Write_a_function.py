8b357be91c68bd583e0aff10f0760cc30e0a5807
Steven Samy <stevensivo27@gmail.com>
2021-07-24 23:05:10 +0200

    solving python Write a function
     in hackerrank

---
+++ hackerrank/6#python_Write_a_function.py
@@ -0,0 +1,17 @@
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+            return False
+        else:
+            return True
+        return True
+    
+    return False
+
+year = int(input())
+print(is_leap(year))
+
