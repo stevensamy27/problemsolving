1075fe5dd819ed388a61938efc601a43e3c174f7
Steven Samy <stevensivo27@gmail.com>
2021-08-17 22:23:12 +0200

    1# challange

---
+++ hackerrank/PYTHON/1#challange.py
@@ -0,0 +1,13 @@
+'''
+Input Format
+    # You do not need to read any input in this challenge.
+Output Format
+    # Print Hello, World! to stdout.
+Sample Output 0
+    # Hello, World!
+'''
+
+        #RESOLVE IT
+
+if __name__ == '__main__':
+    print ("Hello, World!")
