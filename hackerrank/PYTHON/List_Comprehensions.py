453c18c62d6f18034a6b5eac91c93817d1078191
Steven Samy <stevensivo27@gmail.com>
2021-09-06 15:11:42 +0200

    update code

--- hackerrank/PYTHON/List_Comprehensions.py
+++ hackerrank/PYTHON/List_Comprehensions.py
@@ -5,8 +5,8 @@ if __name__ == '__main__':
     n = int(input())
     
 
-for a in range(0,x+1):
-     for b in range(0,y+1):
-          for c in range(0,z+1):
-               if a + b + c != n:
-                    print ([[a,b,c]])
+    print ([[a,b,c] 
+    for a in range(0,x+1)
+    for b in range(0,y+1) 
+    for c in range(0,z+1) 
+    if a + b + c != n ])
