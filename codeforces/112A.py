d7b22ea0fa5bfbbee80683517a2e1f172e1c44b9
Steven Samy <stevensivo27@gmail.com>
2021-09-09 22:17:51 +0200

    solving problem 112A codeforce

---
+++ codeforces/112A.py
@@ -0,0 +1,23 @@
+x = str(input())
+y = str(input())
+x=x.lower()
+y=y.lower()
+
+
+# for i in x:
+#     print (i)
+
+# for j in y:
+#     print (j)
+
+if x==y:
+    print ("0")
+elif x > y:
+    print ("1")
+else :
+    print("-1")    
+
+
+
+# if x==y : 
+#     print("0")
