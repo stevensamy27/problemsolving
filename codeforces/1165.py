c707101d470dac4d858b19668208261942e75e92
Steven Samy <stevensivo27@gmail.com>
2021-10-12 21:00:08 +0200

    update

--- codeforces/1165.py
+++ codeforces/1165.py
@@ -1,11 +1,22 @@
 x = int(input())
 y = [int(x) for x in input().split()]
-z = x - 1
-for i in range(10):
+output = set()
+for i in y:
+        output.add(i)
+print (len(output))
 
-    if z in y:
-        print (z)
-        break
-    else:    
-        z = z-1
+
+
+# x = int(input())
+# y = [int(x) for x in input().split()]
+# z = 1
+# output = 0
+# for i in range(x):
+#     if z in y:
+#        output = output + 1  
+#        z = z + 1  
+#     else:    
+#         z = z + 1   
+# print(output)
+
  
