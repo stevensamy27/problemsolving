47ad75f35be8ceef378bd81c07dfe1dcb93718b1
Steven Samy <stevensivo27@gmail.com>
2021-10-13 11:39:14 +0200

    solving problem 1165B codeforces

--- codeforces/1165.py
+++ codeforces/1165.py
@@ -1,9 +1,21 @@
 x = int(input())
-y = [int(x) for x in input().split()]
-output = set()
-for i in y:
-        output.add(i)
-print (len(output))
+array = [int(x) for x in input().split()]
+
+array.sort()
+
+day = 1
+for i in array:
+    if i >= day:
+        day += 1
+
+print(day - 1)
+
+# x = int(input())
+# y = [int(x) for x in input().split()]
+# output = set()
+# for i in y:
+#         output.add(i)
+# print (len(output))
 
 
 
