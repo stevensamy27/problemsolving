6ac49c8bafa3a107702fc9ba989c13c029ef6ccb
Steven Samy <stevensivo27@gmail.com>
2021-10-26 21:56:59 +0200

    solving Finding_the_percentage hackerrank

--- hackerrank/PYTHON/Finding_the_percentage.py
+++ hackerrank/PYTHON/Finding_the_percentage.py
@@ -1,11 +1,51 @@
+'''
+The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students. 
+Print the average of the marks array for the student name provided, showing 2 places after the decimal.
+'''
+
+
 if __name__ == '__main__':
     n = int(input())
     student_marks = {}
     for _ in range(n):
-        name, *numbers = input().split()
-        scores = list(map(float, numbers))
-        student_marks[name] = scores
+        line = input().split()
+        student_marks[line[0]] = (float(line[1]) + float(line[2]) + float(line[3]))/3
     name = input()
-    
-    # if name in student_marks:
-    print(student_marks)
+    print ('%.2f'%student_marks[name])
+
+
+# if __name__ == '__main__':
+#     n = int(input())
+#     student_marks = {}
+#     for _ in range(n):
+#         name, *numbers = input().split()
+#         scores = list(map(float, numbers))
+#         student_marks[name] = scores
+#     name = input()
+#     # print(student_marks)
+   
+#     if name in student_marks:
+#         x = sum(scores)
+#         print(x)
+#         media  = x/3
+#         print ('%.2f'%media) 
+
+
+
+# if __name__ == '__main__':
+#     n = int(input())
+#     student_marks = {}
+#     for _ in range(n):
+#         name, *numbers = input().split()
+#         scores = list(map(float, numbers))
+#         student_marks[name] = scores
+#     name = input()
+#     # print(student_marks)
+#     # if name == 'Harsh':
+#     #     print('26.50') 
+#     # else:       
+#     if name in student_marks:
+#         x = sum(scores)
+#         # print(x)
+#         media  = x/3
+#         print ('%.2f'%media) 
