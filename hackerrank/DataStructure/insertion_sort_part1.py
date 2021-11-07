ad2425bd01b48b7c056710cb75d0184cf3d83cd9
Steven Samy <stevensivo27@gmail.com>
2021-11-07 22:28:34 +0200

    problem insertion sort part1 hackerRank

---
+++ hackerrank/DataStructure/insertion_sort_part1.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'insertionSort1' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY arr
+#
+
+def insertionSort1(n, arr):
+    for i in range(1, len(arr)):
+        j = i
+        while j > 0 and arr[j] < arr[j - 1]:
+            arr[j - 1], arr[j] = arr[j], arr[j - 1]
+            j = j - 1
+        print(arr)  
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort1(n, arr)
+    for i in range(n):
+        print(arr)  
