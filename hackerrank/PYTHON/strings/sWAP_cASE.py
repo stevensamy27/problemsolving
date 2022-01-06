76590ccf63aba6f62ac133bcd02447f2ab0b93d8
Steven Samy <stevensivo27@gmail.com>
2022-01-06 21:02:27 +0200

    sWAP_cASE

---
+++ hackerrank/PYTHON/strings/sWAP_cASE.py
@@ -0,0 +1,18 @@
+def swap_case(s):
+    formating = s.swapcase()
+    return formating
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
+
+
+
+
+# x = input(str())
+
+# formating = (x[0].lower()+x[1:].upper())
+# print(x)
+# print(formating)
+
