fb2e074e24afee2cc444502816309139ec965825
Steven Samy <stevensivo27@gmail.com>
2022-01-29 21:26:05 +0200

    mutate_string  (slicing)

---
+++ hackerrank/PYTHON/strings/Multations.py
@@ -0,0 +1,13 @@
+def mutate_string(string, position, character):
+    string = s
+    position = int(i)
+    character = c
+    return  string[:position] + character +string[position+1:]
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)    
+
+
