d11de0b85dbe5ef340662783798f3d57ac0cef85
Steven Samy <stevensivo27@gmail.com>
2022-11-29 13:31:20 +0200

    List

---
+++ hackerrank/PYTHON/Lists.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    N = int(input())       
+    myList = list() 
+
+
+    for i in range(N): 
+        order = input()
+        if(order == 'yes'):
+            print("Hello")
+        else: 
+            print("NO")
+        
